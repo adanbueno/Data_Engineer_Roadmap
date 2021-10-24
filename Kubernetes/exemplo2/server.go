e92f56f00afe4e5dbeabf07c44f82c1206927389
adanbueno <adan_bueno@hotmail.com>
2021-10-24 20:24:35 -0300

    feat: ex 2 and 3

---
+++ Kubernetes/exemplo2/server.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":80", nil)
+
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello Brother</h1>"))
+}
